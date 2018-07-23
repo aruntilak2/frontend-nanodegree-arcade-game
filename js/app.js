c3732af6e9e732105ace8f100da78a03e0a7a916
Tilak <aruntilak2@gmail.com>
2018-07-23 14:45:12 -0700

    Start of the game

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -21,6 +21,12 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+const Player = function (){
+ this.sprite = 'images/char-boy.png';
+};
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -28,7 +34,9 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+// const allEnemies = new Enemy;
 // Place the player object in a variable called player
+// const player = new player;
 
 
 
