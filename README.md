3c4ee159e0bdcfac74602a45b9b5cd9288804d70
Tilak <aruntilak2@gmail.com>
2018-08-19 13:55:43 -0700

    Updated from Udacity review

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+
+INSTRUCTIONS
+============
+1. This game can be played by using 4 keys on keyboard. 
+Player can move to all directions by using left, right, up and down keys only.
+2. Any key functions only when the key is released or key up.
+3. Every single move is considered by moving to next related block.
+4. In the event of Player collides with any enemy will bring him back to the starting position.
+
+
+OBJECTIVE OF THE GAME
+====================
+1. Player should be able to reach to the river without colliding the enemy.
+
+
+    WISHES
+==================
+GOOD LUCK
--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var Enemy = function() {
     this.y=55;
     this.step =101;
     this.jump=83;
-    this.speed=150;
+    this.speed=75;
   
   };
   
@@ -77,7 +77,7 @@ var Enemy = function() {
       }
     }
     if (this.y === 55){
-      alert("Congrats")
+      alert("Congrats");
     }
     // win.requestAnimationFrame(main);
   }
@@ -89,77 +89,45 @@ var Enemy = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
   Player.prototype.handleInput = function(direction) {
-    console.log(direction);
   
     if (this.x >= 0 && this.x <= 505) {
       if (direction == "left" && this.x >= -101) {
         this.x -= 101;
-        console.log("Player at = " + this.x);
-        console.log("Enemy X = " + enemy1.x, ", Enemy Y= " + enemy1.y);
         if (this.x < 100) {
           this.x = 0;
         }
       } else if (direction == "right" && this.x <= 505) {
         this.x += 101;
-        console.log("Player at = " + this.x);
-        console.log("Enemy X = " + enemy1.x, ", Enemy Y= " + enemy1.y);
         if (this.x > 404) {
           this.x = 404;
         }
       } else if (direction == "up" && this.y >= -101) {
         this.y -=83 ;
-        console.log("Player at = " + this.y);
-        console.log("Enemy X = " + enemy1.x, ", Enemy Y= " + enemy1.y);
         if (this.y < -60) {
           this.y = -60;
         }
       } else {
-        // if (direction == "down" && this.y <= 101){
         this.y += 83;
-        console.log("Player at = " + this.y);
-        console.log("Enemy X = " + enemy1.x, ", Enemy Y= " + enemy1.y);
         if (this.y > 440) {
           this.y = 440;
         }
-        //   }
       }
     }
   };
   
   // Now instantiate your objects.
   let enemy1 = new Enemy();
-  // enemy1.x = 20;
   enemy1.y = 55;
-  // console.log(enemy1);
-  
-  // enemy1.sprite = 'images/enemy-bug.png';
-  // enemy1.render();
   let enemy2 = new Enemy();
-  // enemy2.x = 50;
   enemy2.y = 55+83;
-  console.log(enemy2);
-  
-  // enemy2.render();
   let enemy3 = new Enemy();
-  // enemy3.x = 100;
   enemy3.y = 55+166;
-  console.log(enemy3);
-  // enemy3.render();
   
   // Place all enemy objects in an array called allEnemies
-  // var enemy = new Enemy;
-  // var allEnemies = [new Enemy()];
   let allEnemies =[];
-
-  // var allEnemies = [enemy1, enemy2, enemy3];  
   allEnemies.push(enemy1, enemy2,enemy3);
-  console.log(allEnemies);
   // Place the player object in a variable called player
   var player = new Player();
-  if (enemy1.x== player.x && enemy1.y==player.y){
-    console.log("collide")
-  }
-  
   // This listens for key presses and sends the keys to your
   // Player.handleInput() method. You don't need to modify this.
   document.addEventListener("keyup", function(e) {
